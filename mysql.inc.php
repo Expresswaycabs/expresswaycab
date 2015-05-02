1f7012e2aefbca794a5d2fddc87f88cc6061b475
Expressway <expresswaycabs@gmail.com>
2015-05-02 19:48:20 +0530

    Update mysql.inc.php

--- mysql.inc.php
+++ mysql.inc.php
@@ -7,4 +7,22 @@ $password = $url["pass"];
 $db = substr($url["path"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
+$active_group = 'default';
+$active_record = TRUE ;
+$db['default']['hostname'] = $server;
+$db['default']['username'] = $username;
+$db['default']['password'] = $password;
+$db['default']['database'] = $db;
+$db['default']['dbdriver'] = 'mysql';
+$db['default']['dbprefix'] = '';
+$db['default']['pconnect'] = TRUE;
+$db['default']['db_debug'] = TRUE;
+$db['default']['cache_on'] = FALSE;
+$db['default']['cachedir'] = '';
+$db['default']['char_set'] = 'utf8';
+$db['default']['dbcollat'] = 'utf8_general_ci';
+$db['default']['swap_pre'] = '';
+$db['default']['autoinit'] = TRUE;
+$db['default']['striction'] = FALSE;
+
 ?>
