b58b9e0340390dd71519366d0f9721d9841272ae
Expressway <expresswaycabs@gmail.com>
2015-05-03 09:44:50 +0530

    Update mysql.inc.php

--- mysql.inc.php
+++ mysql.inc.php
@@ -4,7 +4,7 @@ $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $server = $url["us-cdbr-iron-east-02.cleardb.net"];
 $username = $url["b48d7c722bf68a"];
 $password = $url["903a58ac24087be"];
-$db = substr($url["mysql://b48d7c722bf68a:903a58ac24087be@us-cdbr-iron-east-02.cleardb.net/heroku_79f9ab8d2fe0569?reconnect=true"], 1);
+$db = substr($url["heroku_79f9ab8d2fe0569"], 1);
 
 $dbc = new mysqli($server, $username, $password, $db);
 
