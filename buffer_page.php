7103e62ef9c9111a0b503aa0c346c396e4c6872f
Expressway <expresswaycabs@gmail.com>
2015-05-03 22:38:09 +0530

    Update buffer_page.php

--- buffer_page.php
+++ buffer_page.php
@@ -1,6 +1,6 @@
 <?php
 require('./includes/config.inc.php');
-include('./includes/header1.html');
+include('./includes/header.html');
 require(MYSQL);
 $reg_errors = array();
 $booking_no = $_SESSION['booking_no'];
