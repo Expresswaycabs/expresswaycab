3c5b886b55f117c15e76d132a82ac5718f1e48ff
Expressway <expresswaycabs@gmail.com>
2015-06-07 23:12:25 +0530

    Update mail_handler.php

--- mail_handler.php
+++ mail_handler.php
@@ -14,5 +14,6 @@ if(isset($_POST['submit'])){
     echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // You cannot use header and echo together. It's one or the other.
+    header("Location:thankyou.php");
     }
 ?>
