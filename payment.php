8a3da29d73ad2c1fc69b1764e9310cb76e80ed58
Expressway <expresswaycabs@gmail.com>
2015-06-07 22:26:20 +0530

    Update payment.php

--- payment.php
+++ payment.php
@@ -49,20 +49,23 @@ require(MYSQL);
         </script>
 </head>
 <body>
-Enter your billing information:
+<div class="header">
+<h1 class="logo">PAYMENT GATEWAY</h1>
+</div>		
+<br>Enter your billing information:
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
 <div class="container-fluid">
 
 
 <div class="row">
-<div class="col-sm-4 col-md-4 col-lg-4">
+<div class="col-sm-3 col-md-3 col-lg-3">
 Enter Your Credit Card No:
 </div>
-<div class="col-sm-6 col-md-6 col-lg-6">
+<div class="col-sm-3 col-md-3 col-lg-3">
 <input type="text" name="cardno" placeholder="****-****-****">
 </div>
-<div class="col-sm-2 col-md-2 col-lg-2"></div>
-</div>
+<div class="col-sm-6 col-md-6 col-lg-6"></div>
+</div><br><br>
 
 <div class="row">
 <div class="col-sm-3 col-md-3 col-lg-3">
@@ -80,21 +83,23 @@ CVV:
 </div>
 
 <div class="row">
-<div class="col-sm-4 col-md-4 col-lg-4">
+<div class="col-sm-3 col-md-3 col-lg-3">
 Enter Your Phone No:
 </div>
-<div class="col-sm-6 col-md-6 col-lg-6">
+<div class="col-sm-3 col-md-3 col-lg-3">
 <input type="text" name="phno" placeholder="Enter 10 digit phone no">
 </div>
+<div class="col-sm-6 col-md-6 col-lg-6></div>
 </div>
 
 <div class="row">
-<div class="col-sm-4 col-md-4 col-lg-4">
+<div class="col-sm-3 col-md-3 col-lg-3">
 Enter Your Email No:
 </div>
-<div class="col-sm-6 col-md-6 col-lg-6">
+<div class="col-sm-3 col-md-3 col-lg-3">
 <input type="email" name="email" placeholder="xyz@abc.com">
 </div>
+<div class="col-sm-6 col-md-6 col-lg-6></div>
 </div>
 
 <div class="row">
